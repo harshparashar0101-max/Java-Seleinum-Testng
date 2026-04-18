1dc54ed177cc9cdd79de21808486176114342c44
Harsh Parashar <harsh.parashar0101@gmail.com>
2026-04-18 18:51:32 +0530

    Added Xray annotations and removed mapping file usage

--- src/test/java/com/test/LoginTest.java
+++ src/test/java/com/test/LoginTest.java
@@ -3,9 +3,12 @@ package com.test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import app.getxray.xray.testng.annotations.XrayTest;
+
 public class LoginTest {
 
     @Test
+    @XrayTest(key = "LOGI-80")
     public void verifyLogin() {
         System.out.println("Login test executed");
         Assert.assertTrue(true);
